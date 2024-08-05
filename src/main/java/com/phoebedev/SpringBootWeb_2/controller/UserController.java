d3944d310e705e49e708d6e1e23523a6be8df756
MSI\Phuong Anh <ltpa1203@gmail.com>
2024-08-05 14:13:15 +0700

    Commit validate body, param, path variable

--- src/main/java/com/phoebedev/SpringBootWeb_2/controller/UserController.java
+++ src/main/java/com/phoebedev/SpringBootWeb_2/controller/UserController.java
@@ -1,18 +1,18 @@
 package com.phoebedev.SpringBootWeb_2.controller;
 
 import com.phoebedev.SpringBootWeb_2.dto.request.UserRequestDto;
-import com.phoebedev.SpringBootWeb_2.dto.response.UserResponseDto;
-import org.springframework.stereotype.Controller;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
-    @PostMapping(value = "/", headers = "apiKey=v1.0")
-    public String addUser(@RequestBody UserRequestDto userDto) {
+    @PostMapping(value = "/addUser", headers = "apiKey=v1.0")
+    public String addUser(@Valid @RequestBody UserRequestDto userDto) {
         return "User added successfully";
     }
 
@@ -31,7 +31,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable int userId) {
+    public String deleteUser(@Min(1) @PathVariable int userId) {
         System.out.println("Request delete userID= " + userId);
         return "User deleted successfully";
     }
@@ -39,7 +39,7 @@ public class UserController {
     @GetMapping("/{userId}")
     public UserRequestDto getUser(@PathVariable int userId) {
         System.out.println("Request retrieve userID= " + userId);
-        return new UserRequestDto("acc1", "acc", "a@gmail.com", "123456");
+        return new UserRequestDto("acc1", "acc", "a@gmail.com", "1234567890");
     }
 
     @GetMapping("listUser")
@@ -48,9 +48,9 @@ public class UserController {
             @RequestParam(defaultValue = "10") int pageSize) {
         System.out.println("Request retrieve all users");
         return List.of(
-                new UserRequestDto("acc1", "acc", "a@gmail.com", "123456"),
-                new UserRequestDto("acc2", "acc", "b@gmail.com", "123456"),
-                new UserRequestDto("acc3", "acc", "c@gmail.com", "123456")
+                new UserRequestDto("acc1", "acc", "a@gmail.com", "1234568901"),
+                new UserRequestDto("acc2", "acc", "b@gmail.com", "1234567890"),
+                new UserRequestDto("acc3", "acc", "c@gmail.com", "1234567890")
         );
     }
 }
--- src/main/java/com/phoebedev/SpringBootWeb_2/dto/request/UserRequestDto.java
+++ src/main/java/com/phoebedev/SpringBootWeb_2/dto/request/UserRequestDto.java
@@ -1,19 +1,39 @@
 package com.phoebedev.SpringBootWeb_2.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.phoebedev.SpringBootWeb_2.util.PhoneNumber;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
 
 // dùng Serializable để khi nó có 1 qtr sẽ chuyển đổi từ JSON sang nhị phân và ngược lại
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequestDto implements Serializable {
+    @NotBlank(message = "firstname must be not blank")
     private String firstName;
+    @NotNull(message = "lastname must be not blank")
     private String lastName;
+    @Email(message = "email invalid format")
     private String email;
+//    @Pattern(regexp = "^\\d{10}$", message= "phone invalid format")
+    @PhoneNumber
     private String phone;
+    @NotNull(message = "DOB must be not null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date dob;
+    @NotEmpty
+    List<String> permission;
 
+    public UserRequestDto(String acc1, String acc, String mail, String number) {
+    }
 }
