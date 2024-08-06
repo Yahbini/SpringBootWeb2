e72cc6b6f5eb6dac843b4a58147b322b30ab4ca1
MSI\Phuong Anh <ltpa1203@gmail.com>
2024-08-06 17:02:53 +0700

    commit validate with enum

---
+++ src/main/java/com/phoebedev/SpringBootWeb_2/interfaces/EnumPattern.java
@@ -0,0 +1,20 @@
+package com.phoebedev.SpringBootWeb_2.interfaces;
+
+import com.phoebedev.SpringBootWeb_2.validations.EnumPatternValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER, ElementType.TYPE_USE})
+@Constraint(validatedBy = EnumPatternValidator.class)
+public @interface EnumPattern {
+    String name();
+    String regex();
+    String message() default "{name} must match {regex}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
